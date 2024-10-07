f3399cffe7d421eff10a2ac5824aa1fd8c54e196
Sukju Hong <ehensnfl@gmail.com>
2024-10-07 14:44:44 +0900

    refactor: kotlin-logging root project로 이동

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,6 +44,7 @@ subprojects {
         implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
         implementation("org.jetbrains.kotlin:kotlin-reflect")
         implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+        implementation("io.github.oshai:kotlin-logging-jvm:5.1.4")
         testImplementation("org.springframework.boot:spring-boot-starter-test")
         testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
         testRuntimeOnly("org.junit.platform:junit-platform-launcher")
--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -5,7 +5,3 @@ val bootJar: BootJar by tasks
 
 bootJar.enabled = false
 jar.enabled = true
-
-dependencies {
-    implementation("io.github.oshai:kotlin-logging-jvm:5.1.4")
-}
