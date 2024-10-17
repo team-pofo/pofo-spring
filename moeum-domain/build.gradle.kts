83d26524e38cb63957b1a84e4b210fdd1814b26b
Sukju Hong <ehensnfl@gmail.com>
2024-10-17 18:56:52 +0900

    feat: ProjectEntity 생성
    + StringListConverter 생성

--- moeum-domain/build.gradle.kts
+++ moeum-domain/build.gradle.kts
@@ -8,6 +8,8 @@ jar.enabled = true
 
 dependencies {
     api("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("com.fasterxml.jackson.core:jackson-core:2.18.0")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.18.0")
     implementation("javax.annotation:javax.annotation-api:1.3.2")
     runtimeOnly("org.postgresql:postgresql:42.7.4")
     runtimeOnly("com.h2database:h2:2.3.232")
